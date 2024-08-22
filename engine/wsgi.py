910ff7cdf92d70ca4adc7cbcaa06cc0f75ccbfa1
zygrade009 <vignesh.cheepurapalli@kgpian.iitkgp.ac.in>
2024-08-22 19:01:54 +0530

    messages

--- engine/wsgi.py
+++ engine/wsgi.py
@@ -11,8 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engine.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
 
 app = application
